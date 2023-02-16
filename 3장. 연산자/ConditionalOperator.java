97ad0dfcb5d45dfd4877148309238171a0389828
LLouis <llouis0622@gmail.com>
2023-02-16 15:10:58 +0900

    .

---
+++ 3장. 연산자/ConditionalOperator.java
@@ -0,0 +1,19 @@
+/*»ïÇ× ¿¬»êÀÚ*/
+
+public class ConditionalOperator {
+    public static void main(String[] args) {
+        //#1. »ïÇ×¿¬»êÀÚ
+        int value1 = (3 > 5) ? 6 : 9;
+        System.out.println(value1); //9
+        
+        int value2 = (5 > 3) ? 10 : 20;
+        System.out.println(value2); //10
+        
+        int value3 = 3;
+        System.out.println((value3 % 2 == 0) ? "Â¦¼ö" : "È¦¼ö");
+        		
+        //cf. if - else ±¸¹®À¸·Î º¯È¯
+        if(value3 % 2 == 0) System.out.println("Â¦¼ö");
+        else System.out.println("È¦¼ö");
+    }	
+}
