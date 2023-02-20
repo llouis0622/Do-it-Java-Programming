4932219633d662cb736f0592e8b7fcffeeb3a554
LLouis <llouis0622@gmail.com>
2023-02-20 09:44:09 +0900

    .

---
+++ 5장. 참조 자료형/CreateStringObject.java
@@ -0,0 +1,13 @@
+/* String 按眉 积己狼 滴 啊瘤 规过 */
+
+public class CreateStringObject {
+	public static void main(String[] args) {
+		// #1. String 按眉 积己 1
+		String str1 = new String("救崇");
+		System.out.println(str1);
+		
+		// #2. String 按眉 积己 2
+		String str2 = "救崇窍技夸";
+		System.out.println(str2);
+	}
+}
